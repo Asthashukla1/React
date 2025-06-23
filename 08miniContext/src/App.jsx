52709833e198a804704a60908a5ca55fe1502eac
Astha Shukla <asthashukla160428@gmail.com>
2025-06-23 20:35:49 +0530

    Starting with mini context

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+     <h1>React is Imp</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
