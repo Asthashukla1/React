5b154e70c09a82ec71f11c4645f193e8e8833b32
Astha Shukla <asthashukla160428@gmail.com>
2025-07-15 22:28:36 +0530

    added PostCard

--- 12MegaBlog/src/components/Input.jsx
+++ 12MegaBlog/src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useId } from "react";
 
 const Input = React.forwardRef(function Input(
     {label,
@@ -7,7 +7,25 @@ const Input = React.forwardRef(function Input(
     input,
     ...props
     }, ref){
-    return <h1>my personal input box</h1>
+    const id= useId()
+    return (
+        <div className="w-full">
+            {label && <label
+            className="inline-block mb-1 p1-1"
+            htmlFor={id}>
+            </label>
+            }
+            <input
+            type={text}
+            className={`px-3 py-2 rounded-lg bg-white text-black  outline-none 
+                     focus:bg-gray-50 duration-200 border-gray-200 w-full
+                      ${className}`}
+            ref={ref}
+            {...props}
+            id={id}
+            />
+        </div>
+    )
 })
 
 export default Input
---
+++ 12MegaBlog/src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link, useNavigate} from 'react-router-dom'
+import {login as authLogin} from '../store/authSlice'
+import {Button, Logo, Input} from './index'
+import { useDispatch } from "react-redux";
+import authService from '../appwrite/auth'
+import {useForm} from 'react-router-dom'
+
+function Login(){
+    return(
+        <div>Login</div>
+    )
+}
+
+export default Login
